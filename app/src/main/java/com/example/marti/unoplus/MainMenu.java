d8514bf84dcd0fb22e48e718a09bfb3ffcf4a21d
SebastianAT <setr780@gmail.com>
2018-03-30 12:08:10 +0200

    Game Menu , Grafiken

---
+++ app/src/main/java/com/example/marti/unoplus/MainMenu.java
@@ -0,0 +1,45 @@
+package com.example.marti.unoplus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainMenu extends AppCompatActivity {
+    public MainMenu() {
+        super();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.main_menu);
+
+
+        findViewById(R.id.exitbutton).setOnClickListener(handler);
+        findViewById(R.id.einstellungen).setOnClickListener(handler);
+    }
+
+    View.OnClickListener handler = new View.OnClickListener(){
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+
+                case R.id.einstellungen:
+                    // doStuff
+
+                    break;
+                case R.id.exitbutton:
+                    // doStuff
+                    finish();
+                    break;
+            }
+        }
+    };
+
+
+}
