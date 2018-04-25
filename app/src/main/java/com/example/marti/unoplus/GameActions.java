dc358637bf2886dd0bfa99b73996bd8bd121cb7a
Prepe <37080825+Prepe@users.noreply.github.com>
2018-04-25 18:03:29 +0200

    Added basics to GameActions Class
    Added:
    - enum for the type of action that can be performed
    - 2 Integers for the Players that this actions will effect
    - 1 List for Cards
    - 1 color for the Color wish
    - 1 boolean
    - 4 diferent Constructors to call

---
+++ app/src/main/java/com/example/marti/unoplus/GameActions.java
@@ -0,0 +1,52 @@
+package com.example.marti.unoplus;
+
+import com.example.marti.unoplus.cards.Card;
+
+import java.util.List;
+
+/**
+ * Created by marti on 25.04.2018.
+ */
+
+public class GameActions {
+    public enum actions {
+        PLAY_CARD (0),
+        DRAW_CARD (1),
+        WISH_COLOR (2),
+        DROP_CARD (3),
+        TRADE_CARD (4);
+
+        private int value;
+        private actions(int value){this.value = value;}
+    }
+    public actions action;
+    public Integer playerID;
+    public Integer nextPlayerID;
+    public List<Card> cards;
+    public Boolean check;
+    public Card.colors colorWish;
+
+    public GameActions(actions action, int pID, int nextPID) {
+        this.action = action;
+        playerID = pID;
+        nextPlayerID = nextPID;
+    }
+
+    public GameActions(actions action, int pID, List<Card> drawnCards) {
+        this.action = action;
+        playerID = pID;
+        cards = drawnCards;
+    }
+
+    public GameActions(actions action, int pID, Card.colors color) {
+        this.action = action;
+        playerID = pID;
+        colorWish = color;
+    }
+
+    public GameActions(actions action, int pID, boolean check) {
+        this.action = action;
+        playerID = pID;
+        this.check = check;
+    }
+}
--- app/src/main/java/com/example/marti/unoplus/GameTurn.java
+++
@@ -1,8 +0,0 @@
-package com.example.marti.unoplus;
-
-/**
- * Created by marti on 25.04.2018.
- */
-
-public class GameTurn {
-}
