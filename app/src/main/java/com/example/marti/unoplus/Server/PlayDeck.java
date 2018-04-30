40708944d797dc07e608d70c96e2a466267e4c4e
Ekaterina <ek.zhuravleva@gmail.com>
2018-04-30 14:06:21 +0200

    Created PlayDeck, ServerLogic and TakeDeck classes.

---
+++ app/src/main/java/com/example/marti/unoplus/Server/PlayDeck.java
@@ -0,0 +1,18 @@
+package com.example.marti.unoplus.Server;
+
+import com.example.marti.unoplus.cards.Card;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ekzhu on 30.04.2018.
+ */
+
+public class PlayDeck {
+    ArrayList<Card> cards = null;
+
+    public PlayDeck()
+    {
+        this.cards = new ArrayList<Card>();
+    }
+}
---
+++ app/src/main/java/com/example/marti/unoplus/Server/ServerLogic.java
@@ -0,0 +1,19 @@
+package com.example.marti.unoplus.Server;
+
+/**
+ * Created by ekzhu on 30.04.2018.
+ */
+
+public class ServerLogic {
+
+    public ServerLogic()
+    {
+        this.takedeck = new TakeDeck();
+        this.takedeck.CreateCards();
+        this.playdeck = new PlayDeck();
+    }
+
+    public int startingHand = 7;
+    public TakeDeck takedeck = null;
+    public PlayDeck playdeck = null;
+}
