c227025ce21d40e9eb4d4f422503728497e4c9c9
Prepe <37080825+Prepe@users.noreply.github.com>
2018-05-09 14:19:42 +0200

    cleanup Player Class
    cleand Player Class
    added Case UPDATE to callPlayer

--- app/src/main/java/com/example/marti/unoplus/players/Player.java
+++ app/src/main/java/com/example/marti/unoplus/players/Player.java
@@ -1,13 +1,5 @@
 package com.example.marti.unoplus.players;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.example.marti.unoplus.GameActions;
 import com.example.marti.unoplus.cards.Card;
 import com.example.marti.unoplus.gameLogicImpl.GameController;
@@ -20,42 +12,12 @@ import jop.hab.net.NetworkIOManager;
 import jop.hab.net.ObserverInterface;
 
 public class Player {
-   NetworkIOManager networkIOManager;
-    TextView textView;
-    EditText editTextSend;
-    Button btnSend;
-    Button btnUnoUno;
-    ImageButton btnr8;
-    ImageButton btng7;
-    ImageButton btnb3;
-    ImageView playCard;
-    String hostAdress;
-    String mode;
-
-/*
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-        hostAdress = getIntent().getStringExtra("adress");
-        mode = getIntent().getStringExtra("mode");
-
-
-        networkIOManager = new NetworkIOManager(this);
-        networkIOManager.setMode(mode);
-        networkIOManager.setHostAdress(hostAdress);
-        networkIOManager.open();
-
-
-    }
-*/
-
     static String name;
     static int ID;
     public GameController gameController;
     public static boolean cheated = true;
     GameViewProt gameViewProt;
+    Card lastCard;
 
     LinkedList<Card> cards; //Hand
 
@@ -81,21 +43,52 @@ public class Player {
         return ID;
     }
 
-    void drawCard(){
+    //<---------- Player Actions ---------->
+    public void drawCard(){
         GameActions action;
         action = new GameActions(GameActions.actions.DRAW_CARD, ID);
         gameViewProt.updateAllPlayers(action);
     }
-/*
-    void sendAction(GameActions actions){
-        networkIOManager.writeGameaction(actions);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+    public void playCard(Card c){
+        GameActions action;
+        action = new GameActions(GameActions.actions.PLAY_CARD, ID, c);
+        gameViewProt.updateAllPlayers(action);
+    }
+
+    public void dropCard(Card c){
+
+    }
+
+    public void TradeCard(Card c, Player p){
+
+    }
+
+    //<---------- Player Reactions ---------->
+    public void callPlayer(GameActions action) {
+        switch(action.action){
+            case DRAW_CARD:
+                gotCard(action.playerID, action.cards);
+                break;
+            case PLAY_CARD:
+                cardPlayed(action.playerID, action.card);
+                break;
+            case UPDATE:
+                updateGame(action.card);
+                break;
+            case NEXT_PLAYER:
+                //TODO ipml
+                break;
         }
     }
-*/
+
+    boolean checkID(int ID){
+        return ID == this.ID;
+    }
+
+    void updateGame(Card lastCard) {
+        this.lastCard = lastCard;
+    }
 
     void gotCard(int ID, List<Card> cards) {
         if (checkID(ID)) {
@@ -105,62 +98,9 @@ public class Player {
         }
     }
 
-    boolean checkID(int ID){
-        return ID == this.ID;
-    }
-
-    void playCard(Card c){
-        GameActions action;
-        action = new GameActions(GameActions.actions.PLAY_CARD, ID, c);
-        gameViewProt.updateAllPlayers(action);
-    }
-
-    void dropCard(Card c){
-        /*
-        if(gameController.dropCard()) {
-            cards.remove(c);
-        }
-        */
-    }
-
-    void TradeCard(Card c, Player p){
-        cards.remove(c);
-        c = gameController.tradeCard(p,c);
-        cards.add(c);
-    }
-
     void cardPlayed(int ID, Card card){
         if (checkID(ID)) {
             this.cards.remove(card);
         }
     }
-
-    public void callPlayer(GameActions action) {
-        switch(action.action){
-            case DRAW_CARD:
-                gotCard(action.playerID, action.cards);
-                break;
-            case PLAY_CARD:
-                cardPlayed(action.playerID, action.card);
-        }
-    }
-
-    /**
-    @Override
-    public void dataChanged() {
-        GameActions action;
-        action = networkIOManager.getGameAction();
-
-        switch(action.action){
-            case DRAW_CARD:
-                gotCard(action.playerID, action.cards);
-                break;
-            case PLAY_CARD:
-                cardPlayed(action.playerID, action.card);
-        }
-
-
-    }
-**/
-
 }
