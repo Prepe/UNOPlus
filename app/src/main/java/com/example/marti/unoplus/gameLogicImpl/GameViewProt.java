79a8373437d07980e3bd9bc462d01dbabd4ce2f8
Prepe <37080825+Prepe@users.noreply.github.com>
2018-05-16 15:59:44 +0200

    readdding lost content in GVP

--- app/src/main/java/com/example/marti/unoplus/gameLogicImpl/GameViewProt.java
+++ app/src/main/java/com/example/marti/unoplus/gameLogicImpl/GameViewProt.java
@@ -6,11 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.example.marti.unoplus.GameActions;
 import com.example.marti.unoplus.GameStatics;
 import com.example.marti.unoplus.R;
+import com.example.marti.unoplus.cards.Card;
 import com.example.marti.unoplus.cards.HandCardView;
+import com.example.marti.unoplus.cards.PlayedCardView;
 import com.example.marti.unoplus.players.Player;
 import com.example.marti.unoplus.players.PlayerList;
 
@@ -28,10 +31,11 @@ public class GameViewProt extends AppCompatActivity implements ObserverInterface
     Player player;
     GameActions recievedGA;
     ArrayList<HandCardView> handCards;
+    PlayedCardView playedCardView;
 
     public GameViewProt() {
         super();
-        this.handCards = new ArrayList<HandCardView>();
+        this.handCards = new ArrayList<>();
     }
 
     @Override
@@ -148,4 +152,73 @@ public class GameViewProt extends AppCompatActivity implements ObserverInterface
             player.callPlayer(action);
         }
     }
+
+    //<--------- View Updates --------->
+    //Player sends an action
+    public void writeNetMessage(GameActions action)
+    {
+        Log.d("player","playeraction");
+        this.NIOmanager.writeGameaction(action);
+    }
+
+    //Update the view to show the last played card
+    public void updateCurrentPlayCard(Card card)
+    {
+        this.playedCardView.updateCard(card);
+    }
+
+    //Visualy add Cards to player hand
+    public void addCardToHand(Card card) {
+        //soundManager.playSound(Sounds.DRAWCARD);
+        HandCardView cardview = new HandCardView(GameViewProt.this, this, card);
+        this.handCards.add(cardview);
+
+
+        int numCardshand = 0;
+        for (int i = 0; i < handCards.size(); i++) {
+
+            numCardshand++;
+        }
+
+        String s = Integer.toString(numCardshand);
+        System.out.println(numCardshand);
+        //numCards.setText("( " + s + " )");
+
+
+        LinearLayout handBox = findViewById(R.id.playerHandLayout);
+        handBox.addView(cardview.view);
+    }
+
+    //Visualy remove Cards to player hand
+    public void removeCardFromHand(Card card) {
+        if (GameStatics.devMode) {
+            Log.d("GameDebug", "Gamescreen tries to remove following card from hand :" + card.value.toString() + " " + card.color.toString());
+        }
+
+        LinearLayout handBox = findViewById(R.id.playerHandLayout);
+        for (HandCardView c : this.handCards) {
+            if (c.card.hasSameCardValueAs(card)) {
+                Log.d("GameDebug", "Found it in players hand!");
+
+                handBox.removeView(c.view);
+                this.handCards.remove(c);
+                //soundManager.playSound(Sounds.DROPCARD);
+                //timer.start();
+
+                int numCardshand = 0;
+                for (int i = 0; i < handCards.size(); i++) {
+
+                    numCardshand++;
+                }
+
+                String s = Integer.toString(numCardshand);
+                System.out.println(numCardshand);
+                //numCards.setText("( " + s + " )");
+
+                return;
+            }
+        }
+        Log.d("GameDebug", "Didn't find it.... fix plz");
+    }
+
 }
