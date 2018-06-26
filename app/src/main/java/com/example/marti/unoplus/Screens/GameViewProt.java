60a63cdd0f6be4692a08c50283a75fe10cf08180
Martin <37080825+Prepe@users.noreply.github.com>
2018-06-26 22:37:01 +0200

    CallUNO fix Playerside

--- app/src/main/java/com/example/marti/unoplus/Screens/GameViewProt.java
+++ app/src/main/java/com/example/marti/unoplus/Screens/GameViewProt.java
@@ -733,4 +733,14 @@ public class GameViewProt extends AppCompatActivity implements ObserverInterface
     public void toastPlayersTime() {
         Toast.makeText(getApplicationContext(), "Deine Zeit: " + player.getMillSecs() + " Millisekunden", Toast.LENGTH_SHORT).show();
     }
+
+    public void callUNO(int id, boolean check) {
+        if (check) {
+            if (id != player.getID()) {
+                Toast.makeText(getApplicationContext(), "Player " + (player.getID() + 1) + " called UNO", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Player " + (player.getID() + 1) + " called UNO and has drawn a Card", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
